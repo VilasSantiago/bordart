3d8307ade535f69f5a30704672788a122ad6fa02
VilasSantiago <santiagovilasrn@gmail.com>
2025-01-13 13:42:13 -0300

    navigationBar + config

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  content: ['./src/**/*.{astro,js,jsx,ts,tsx,html}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
