d831dc973b74a6ee6726b47616ff140e557daf69
VilasSantiago <santiagovilasrn@gmail.com>
2025-05-08 19:15:35 -0300

    FINISH ?...

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface Producto {
+    id: string;
+    nombre: string;
+    descripcion: string;
+    precio: number;
+    imagenes: string[];
+    colores: string[];
+    talles: string[];
+    tablaTalles: string;
+  }
+  
